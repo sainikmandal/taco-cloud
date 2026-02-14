5fc748dc5829d01f27834b9e69dfc4127dced825
sainikm <isainikkhan@gmail.com>
2026-02-15 01:49:46 +0530

    Refactor: replace HomeController with WebConfig view controller
    - Create config package with WebConfig implementing WebMvcConfigurer
    - Register '/' route using addViewControllers instead of @Controller
    - Update HomeControllerTest to test WebConfig configuration
    - Remove HomeController as it's no longer needed
    
    This simplifies the home page routing by using Spring's built-in view controller registry.

--- src/test/java/com/sainik/tacocloud/HomeControllerTest.java
+++ src/test/java/com/sainik/tacocloud/HomeControllerTest.java
@@ -1,22 +1,25 @@
 package com.sainik.tacocloud;
 
-import com.sainik.tacocloud.controllers.HomeController;
+import com.sainik.tacocloud.config.WebConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.containsString;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
+@Import(WebConfig.class)
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Test
-    public void TestHomePage() throws Exception {
+    public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
