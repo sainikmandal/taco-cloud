7d8384e24237d5c8e837e59cb9e5a0b92c228a1a
sainikm <isainikkhan@gmail.com>
2026-02-14 20:52:57 +0530

    Initial commit: Taco Cloud Spring Boot app

---
+++ src/test/java/com/sainik/tacocloud/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package com.sainik.tacocloud;
+
+import com.sainik.tacocloud.controllers.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void TestHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+}
